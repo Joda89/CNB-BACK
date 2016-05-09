95d41d0895008b61370ac190b167fd074d0da722
Joda <johan.lore@free.fr>
2016-05-09 23:06:43 +0200

    Mise en place des services User mail phone et adresse

--- resources/config/prod.php
+++ resources/config/prod.php
@@ -4,8 +4,8 @@ $app['api.version'] = "v1";
 $app['api.endpoint'] = "/api";
 $app['db.options'] = array(
   "driver" => "pdo_mysql",
-  "user" => "userN7C",
-  "password" => "XrEkC3ShxlSInN48",
-  "dbname" => "sampledb",
-  "host" => "10.1.1.3",
+  "user" => "userLAB",
+  "password" => "kRBJNPreArG1hlNs",
+  "dbname" => "cnb",
+  "host" => "10.1.0.7",
 );
