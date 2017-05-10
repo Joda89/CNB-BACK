a6f52bb390035e2f7f1304173b34429972d9cfd7
Joda <johan.lore@free.fr>
2017-05-10 17:20:28 +0200

    Mise en place du systeme d'env + database

--- index.php
+++ index.php
@@ -6,12 +6,12 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $app = new Silex\Application();
 
-printf(getenv("ENV"));
+echo getenv("ENV") ;
 
 //if(getenv("ENV") == 'prod') {
     require __DIR__ . '/resources/config/prod.php';
 //} else {
-    require __DIR__ . '/resources/config/dev.php';
+//    require __DIR__ . '/resources/config/dev.php';
 //}
 
 require __DIR__ . '/src/App.php';
